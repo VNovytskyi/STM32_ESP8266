f0fca9d50db430a6228f8f1b15f1f989d13789f6
VNovytskyi <vladyslavnovytskyi2000@gmail.com>
2019-12-17 16:50:59 +0200

    New version of code organization

--- ESP8266.c
+++ ESP8266.c
@@ -6,34 +6,50 @@ char ESP_TX_buff[ESP_TX_buff_size];
 
 extern UART_HandleTypeDef huart2;
 
-int ESP8266_Test(void)
+void ESP8266_Error(char *errorMessage)
+{
+    PC_Send(errorMessage);
+}
+
+void ESP8266_Test(void)
 {
 	char *str = "AT\r\n";
     
 	HAL_UART_Transmit(&huart2,(uint8_t*)str, strlen(str), 100);
 	
 	HAL_UART_Receive(&huart2, (uint8_t *)bool_answer, ESP_BOOL_ANSWER_SIZE, 100);
-	
-	return strstr(bool_answer, "OK") == NULL? ESP8266_TEST_ERROR: ESP8266_TEST_OK;
+    
+    if(strstr(bool_answer, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_TEST_ERROR");
+    }
 }
 
-int ESP8266_EnableEcho()
+void ESP8266_EnableEcho()
 {
     char *str2 = "ATE1\r\n";
     HAL_UART_Transmit(&huart2,(uint8_t*)str2, strlen(str2), 100);
     HAL_UART_Receive(&huart2, (uint8_t *)bool_answer, ESP_BOOL_ANSWER_SIZE, 100);
-    return strstr(bool_answer, "OK") == NULL? ESP8266_ENABLE_ECHO_ERROR: ESP8266_ENABLE_ECHO_OK;
+    
+    if(strstr(bool_answer, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_ENABLE_ECHO_ERROR");
+    }
 }
 
-int ESP8266_DisableEcho()
+void ESP8266_DisableEcho()
 {
     char *str1 = "ATE0\r\n";
     HAL_UART_Transmit(&huart2,(uint8_t*)str1, strlen(str1), 100);
     HAL_UART_Receive(&huart2, (uint8_t *)bool_answer, ESP_BOOL_ANSWER_SIZE, 100);
-    return strstr(bool_answer, "OK") == NULL? ESP8266_DISABLE_ECHO_ERROR: ESP8266_DISABLE_ECHO_OK;
+    
+    if(strstr(bool_answer, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_DISABLE_ECHO_ERROR");
+    }
 }
 
-int ESP8266_ConnectTo(char *wifiName, char *password)
+void ESP8266_ConnectTo(char *wifiName, char *password)
 {
 	sprintf(ESP_TX_buff, "AT+CWJAP_CUR=\"%s\",\"%s\"\r\n", wifiName, password);
 	HAL_UART_Transmit(&huart2,(uint8_t*)ESP_TX_buff, strlen(ESP_TX_buff), 100);
@@ -45,11 +61,14 @@ int ESP8266_ConnectTo(char *wifiName, char *password)
 		HAL_UART_Receive(&huart2, (uint8_t *)ESP_RX_buff, ESP_RX_buff_size, 100);
         
 	}while(strstr(ESP_RX_buff, "OK") == NULL && strstr(ESP_RX_buff, "ERROR") == NULL);
-	
-	return strstr(ESP_RX_buff, "OK") == NULL? ESP8266_WIFI_CONNECT_ERROR: ESP8266_WIFI_CONNECT_OK;
+    
+    if(strstr(ESP_RX_buff, "OK") == NULL)
+    {
+       ESP8266_Error("[ ERROR ]Connect to MERCUSYS_7EBA\n");
+    }
 }
 
-int ESP8266_DisconnectFromWifi()
+void ESP8266_DisconnectFromWifi()
 {
 	char *str = "AT+CWQAP\r\n";
 	HAL_UART_Transmit(&huart2,(uint8_t*)str, strlen(str), 100);
@@ -61,12 +80,14 @@ int ESP8266_DisconnectFromWifi()
 		HAL_UART_Receive(&huart2, (uint8_t *)ESP_RX_buff, ESP_RX_buff_size, 100);
 		
 	}while(strstr(ESP_RX_buff, "OK") == NULL && strstr(ESP_RX_buff, "ERROR") == NULL);
-	
-	return strstr(ESP_RX_buff, "OK") == NULL? ESP8266_WIFI_DISCONNECT_ERROR: ESP8266_WIFI_DISCONNECT_OK;
+    
+    if(strstr(ESP_RX_buff, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_WIFI_DISCONNECT_ERROR");
+    }
 }
 
-
-int ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request)
+void ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request)
 {
 	sprintf(ESP_TX_buff, "AT+CIPSTART=\"%s\",\"%s\",%d\r\n", type, ip, port);
 	HAL_UART_Transmit(&huart2,(uint8_t*)ESP_TX_buff, strlen(ESP_TX_buff), 100);
@@ -78,9 +99,10 @@ int ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request)
 		
 	}while(strstr(ESP_RX_buff, "OK") == NULL && strstr(ESP_RX_buff, "ERROR") == NULL);
 	
-	if(strstr(ESP_RX_buff, "OK") == NULL)
-		return ESP8266_AT_CIPSTART_ERROR;
-	
+    if(strstr(ESP_RX_buff, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_AT_CIPSTART_ERROR");
+    }
 	
 	sprintf(ESP_TX_buff, "AT+CIPSEND=%d\r\n", strlen(request) + 2);
 	HAL_UART_Transmit(&huart2,(uint8_t*)ESP_TX_buff, strlen(ESP_TX_buff), 100);
@@ -92,8 +114,12 @@ int ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request)
 		
 	}while(strstr(ESP_RX_buff, "OK") == NULL && strstr(ESP_RX_buff, "ERROR") == NULL);
 	
-	if(strstr(ESP_RX_buff, ">") == NULL)
-		return ESP8266_AT_CIPSEND_ERROR;
+    
+    if(strstr(ESP_RX_buff, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_AT_CIPSEND_ERROR");
+        return;
+    }
 	
 	sprintf(ESP_TX_buff, "%s\r\n", request);
 	HAL_UART_Transmit(&huart2,(uint8_t*)ESP_TX_buff, strlen(ESP_TX_buff), 100);
@@ -104,8 +130,9 @@ int ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request)
 		
 	}while(strstr(ESP_RX_buff, "OK") == NULL && strstr(ESP_RX_buff, "ERROR") == NULL);
 
-	if(strstr(ESP_RX_buff, "OK") == NULL)
-		return ESP8266_SEND_REQUEST_ERROR;
-	
-	return ESP8266_SEND_REQUEST_OK;
+    if(strstr(ESP_RX_buff, "OK") == NULL)
+    {
+        ESP8266_Error("[ ERROR ] ESP8266_SEND_REQUEST_ERROR");
+        return;
+    }
 }
--- ESP8266.h
+++ ESP8266.h
@@ -30,16 +30,18 @@
 #define ESP8266_SEND_REQUEST_ERROR 12
 #define ESP8266_SEND_REQUEST_OK 12
 
-int ESP8266_Test(void);
+void ESP8266_Error(char *errorMessage);
 
-int ESP8266_EnableEcho();
+void ESP8266_Test(void);
 
-int ESP8266_DisableEcho();
+void ESP8266_EnableEcho();
 
-int ESP8266_ConnectTo(char *wifiName, char *password);
+void ESP8266_DisableEcho();
 
-int ESP8266_DisconnectFromWifi();
+void ESP8266_ConnectTo(char *wifiName, char *password);
 
-int ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request);
+void ESP8266_DisconnectFromWifi();
+
+void ESP8266_SendRequest(char *type, char *ip, uint8_t port, char *request);
 
 #endif /* ESP8266_H_ */
